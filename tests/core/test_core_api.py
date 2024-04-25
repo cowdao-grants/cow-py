15df625c197c566b3b7d2fcd7a00f22df226cb43
José Ribeiro <me@joseribeiro.dev>
2024-04-25 08:32:14 -0300

    remove stale subgraphs/core modules

--- tests/core/test_core_api.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from cow_py.common import constants, cow_error, chains, config
-
-
-@pytest.mark.parametrize("module", [constants, cow_error, chains, config])
-def test_module_existence(module):
-    assert module is not None
