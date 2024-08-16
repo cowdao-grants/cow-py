08f1d0391981c9c2378e4d20fa168310f70bdc37
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-08-16 12:01:55 -0300

    add appDataHex object and its convertion to cid

--- cow_py/app_data/appDataHex.py
+++ cow_py/app_data/appDataHex.py
@@ -1,6 +1,7 @@
+from typing import Dict, Any
 from web3 import Web3
-from cid import make_cid
-import multihash
+from multiformats import multibase, CID
+
 from cow_py.app_data.appDataCid import AppDataCid
 from cow_py.app_data.consts import MetaDataError
 
@@ -9,30 +10,54 @@ class AppDataHex:
     def __init__(self, app_data_hex: str):
         self.app_data_hex = app_data_hex
 
-    async def to_cid(self) -> AppDataCid:
-        cid = await self._app_data_hex_to_cid()
-        await self._assert_cid(cid)
+    def to_cid(self) -> AppDataCid:
+        cid = self._app_data_hex_to_cid()
+        self._assert_cid(cid)
         return AppDataCid(cid)
 
-    async def to_cid_legacy(self) -> AppDataCid:
-        cid = await self._app_data_hex_to_cid_legacy()
-        await self._assert_cid(cid)
+    def to_cid_legacy(self) -> AppDataCid:
+        cid = self._app_data_hex_to_cid_legacy()
+        self._assert_cid(cid)
         return AppDataCid(cid)
 
-    async def _assert_cid(self, cid: str):
+    def _assert_cid(self, cid: str):
         if not cid:
             raise MetaDataError(
                 f"Error getting CID from appDataHex: {self.app_data_hex}"
             )
 
-    async def _app_data_hex_to_cid(self) -> str:
-        hash_bytes = Web3.to_bytes(hexstr=self.app_data_hex)
-        mh = multihash.encode(hash_bytes, 'keccak-256')
-        cid = make_cid(1, 'raw', mh)
-        return str(cid)
+    def _app_data_hex_to_cid(self) -> str:
+        cid_bytes = self._to_cid_bytes(
+            {
+                "version": 0x01,  # CIDv1
+                "multicodec": 0x55,  # Raw codec
+                "hashing_algorithm": 0x1B,  # keccak hash algorithm
+                "hashing_length": 32,  # keccak hash length (0x20 = 32)
+                "multihash_hex": self.app_data_hex,  # 32 bytes of the keccak256 hash
+            }
+        )
+        return multibase.encode(cid_bytes, "base16")
 
-    async def _app_data_hex_to_cid_legacy(self) -> str:
-        hash_bytes = Web3.to_bytes(hexstr=self.app_data_hex)
-        mh = multihash.encode(hash_bytes, 'sha2-256')
-        cid = make_cid(0, 'dag-pb', mh)
-        return str(cid)
+    def _app_data_hex_to_cid_legacy(self) -> str:
+        cid_bytes = self._to_cid_bytes(
+            {
+                "version": 0x01,  # CIDv1
+                "multicodec": 0x70,  # dag-pb
+                "hashing_algorithm": 0x12,  # sha2-256 hash algorithm
+                "hashing_length": 32,  # SHA-256 length (0x20 = 32)
+                "multihash_hex": self.app_data_hex,  # 32 bytes of the sha2-256 hash
+            }
+        )
+        return str(CID.decode(cid_bytes).set(version=0))
+
+    def _to_cid_bytes(self, params: Dict[str, Any]) -> bytes:
+        hash_bytes = Web3.to_bytes(hexstr=params["multihash_hex"])
+        cid_prefix = bytes(
+            [
+                params["version"],
+                params["multicodec"],
+                params["hashing_algorithm"],
+                params["hashing_length"],
+            ]
+        )
+        return cid_prefix + hash_bytes
