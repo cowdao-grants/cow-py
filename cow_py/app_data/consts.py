afa6893f4b0f32f1aa1ec03f4bfc04c9061c562e
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-08-26 15:14:25 -0300

    enable env modify ipfs urls

--- cow_py/app_data/consts.py
+++ cow_py/app_data/consts.py
@@ -1,5 +1,7 @@
-DEFAULT_IPFS_READ_URI = "https://cloudflare-ipfs.com/ipfs"
-DEFAULT_IPFS_WRITE_URI = "https://api.pinata.cloud"
+import os
+
+DEFAULT_IPFS_READ_URI = os.getenv("IPFS_READ_URI", "https://cloudflare-ipfs.com/ipfs")
+DEFAULT_IPFS_WRITE_URI = os.getenv("IPFS_WRITE_URI", "https://api.pinata.cloud")
 
 
 class MetaDataError(Exception):
