f97c695fd15bd705e171b0a2d838d04c20de1ef9
José Ribeiro <me@joseribeiro.dev>
2024-09-10 10:54:25 -0300

    tests(e2e): use gnosis mainnet for e2e test

--- tests/e2e/test_post_and_cancel_order_live_e2e.py
+++ tests/e2e/test_post_and_cancel_order_live_e2e.py
@@ -1,4 +1,7 @@
-import logging
+# To run this test you will need to fill the .env file with the necessary variables (see .env.example).
+# You will also need to have enough funds in you wallet of the sell token to create the order.
+# The funds have to already be approved to the CoW Swap Vault Relayer
+
 import os
 from dataclasses import asdict
 
@@ -27,34 +30,34 @@ from cow_py.order_book.generated.model import (
     TokenAmount,
 )
 
-# Setup logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
-E2E_SEPOLIA_TESTING_EOA_PRIVATE_KEY = os.getenv(
-    "E2E_SEPOLIA_TESTING_EOA_PRIVATE_KEY", ""
+E2E_GNOSIS_MAINNET_TESTING_EOA_PRIVATE_KEY = os.getenv(
+    "E2E_GNOSIS_MAINNET_TESTING_EOA_PRIVATE_KEY", ""
 )
 
-COW_TOKEN_SEPOLIA_ADDRESS = "0x0625aFB445C3B6B7B929342a04A22599fd5dBB59"
-WETH_SEPOLIA_ADDRESS = "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14"
+COW_TOKEN_GNOSIS_MAINNET_ADDRESS = "0x177127622c4A00F3d409B75571e12cB3c8973d3c"
+WXDAI_GNOSIS_MAINNET_ADDRESS = "0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d"
 
-E2E_SEPOLIA_TESTING_EOA = Account.from_key(E2E_SEPOLIA_TESTING_EOA_PRIVATE_KEY)
-E2E_SEPOLIA_TESTING_EOA_ADDRESS: Address = E2E_SEPOLIA_TESTING_EOA.address
+E2E_GNOSIS_MAINNET_TESTING_EOA = Account.from_key(
+    E2E_GNOSIS_MAINNET_TESTING_EOA_PRIVATE_KEY
+)
+E2E_GNOSIS_MAINNET_TESTING_EOA_ADDRESS: Address = E2E_GNOSIS_MAINNET_TESTING_EOA.address
 
 
-@pytest.mark.vcr
 @pytest.mark.asyncio
 async def test_post_and_cancel_order_live_e2e():
     order_book_api = OrderBookApi(
-        config=OrderBookAPIConfigFactory.get_config("prod", SupportedChainId.SEPOLIA)
+        config=OrderBookAPIConfigFactory.get_config(
+            "prod", SupportedChainId.GNOSIS_CHAIN
+        )
     )
 
     mock_order_quote_request = OrderQuoteRequest(
         **{
-            "sellToken": COW_TOKEN_SEPOLIA_ADDRESS,
-            "buyToken": WETH_SEPOLIA_ADDRESS,
-            "receiver": E2E_SEPOLIA_TESTING_EOA_ADDRESS,
-            "from": E2E_SEPOLIA_TESTING_EOA_ADDRESS,
+            "sellToken": WXDAI_GNOSIS_MAINNET_ADDRESS,
+            "buyToken": COW_TOKEN_GNOSIS_MAINNET_ADDRESS,
+            "receiver": E2E_GNOSIS_MAINNET_TESTING_EOA_ADDRESS,
+            "from": E2E_GNOSIS_MAINNET_TESTING_EOA_ADDRESS,
             "onchainOrder": False,
         }
     )
@@ -70,11 +73,11 @@ async def test_post_and_cancel_order_live_e2e():
 
     order = Order(
         **{
-            "sell_token": COW_TOKEN_SEPOLIA_ADDRESS,
-            "buy_token": WETH_SEPOLIA_ADDRESS,
-            "receiver": E2E_SEPOLIA_TESTING_EOA_ADDRESS,
-            "sell_amount": (10**16).__str__(),
-            "buy_amount": (10**16).__str__(),
+            "sell_token": WXDAI_GNOSIS_MAINNET_ADDRESS,
+            "buy_token": COW_TOKEN_GNOSIS_MAINNET_ADDRESS,
+            "receiver": E2E_GNOSIS_MAINNET_TESTING_EOA_ADDRESS,
+            "sell_amount": (10**15).__str__(),
+            "buy_amount": (10**20).__str__(),
             "valid_to": quote.quote.validTo,
             "fee_amount": "0",
             "kind": "sell",
@@ -86,12 +89,12 @@ async def test_post_and_cancel_order_live_e2e():
     )
 
     order_domain = domain(
-        chain=Chain.SEPOLIA,
+        chain=Chain.GNOSIS,
         verifying_contract=CowContractAddress.SETTLEMENT_CONTRACT.value,
     )
 
     signature = sign_order(
-        order_domain, order, E2E_SEPOLIA_TESTING_EOA, SigningScheme.EIP712
+        order_domain, order, E2E_GNOSIS_MAINNET_TESTING_EOA, SigningScheme.EIP712
     )
 
     order_uid = await order_book_api.post_order(
@@ -105,7 +108,10 @@ async def test_post_and_cancel_order_live_e2e():
     )
 
     order_cancellation_signature = sign_order_cancellation(
-        order_domain, order_uid.root, E2E_SEPOLIA_TESTING_EOA, SigningScheme.EIP712
+        order_domain,
+        order_uid.root,
+        E2E_GNOSIS_MAINNET_TESTING_EOA,
+        SigningScheme.EIP712,
     )
 
     cancellation_result = await order_book_api.delete_order(
