5cfded7261ebf74591c42ab5da12cc9651e6c22e
Peter Jung <peter@jung.ninja>
2025-01-13 14:28:01 +0100

    Implement swap_tokens for easy end-to-end swapping (#22)
    * Implement swap_tokens for easy end-to-end swapping

---
+++ cow_py/__init__.py
@@ -0,0 +1,3 @@
+from cow_py.cow.swap import swap_tokens
+
+__all__ = ["swap_tokens"]
--- examples/order_posting_e2e.py
+++ examples/order_posting_e2e.py
@@ -2,41 +2,22 @@
 # You will also need to have enough funds in you wallet of the sell token to create the order.
 # The funds have to already be approved to the CoW Swap Vault Relayer
 
-import asyncio
-import json
 import os
-
+import asyncio
 from dotenv import load_dotenv
-from web3 import Account
-
+from web3 import Account, Web3
+from web3.types import Wei
+from cow_py.cow.swap import swap_tokens
 from cow_py.common.chains import Chain
-from cow_py.common.config import SupportedChainId
-from cow_py.common.constants import CowContractAddress
-from cow_py.contracts.domain import domain
-from cow_py.contracts.order import Order
-from cow_py.contracts.sign import EcdsaSignature, SigningScheme
-from cow_py.contracts.sign import sign_order as _sign_order
-from cow_py.order_book.api import OrderBookApi
-from cow_py.order_book.config import OrderBookAPIConfigFactory
-from cow_py.order_book.generated.model import (
-    OrderQuoteSideKindSell,
-    OrderQuoteSide1,
-    OrderCreation,
-    OrderQuoteRequest,
-    OrderQuoteResponse,
-    UID,
-    TokenAmount,
-)
 
-BUY_TOKEN = "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14"  # WETH
-SELL_TOKEN = "0xbe72E441BF55620febc26715db68d3494213D8Cb"  # USDC
-SELL_AMOUNT_BEFORE_FEE = "5000000000000000000"  # 50 USDC with 18 decimals
-ORDER_KIND = "sell"
+BUY_TOKEN = Web3.to_checksum_address(
+    "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14"
+)  # WETH
+SELL_TOKEN = Web3.to_checksum_address(
+    "0xbe72E441BF55620febc26715db68d3494213D8Cb"
+)  # USDC
+SELL_AMOUNT_BEFORE_FEE = Wei(5000000000000000000)  # 50 USDC with 18 decimals
 CHAIN = Chain.SEPOLIA
-CHAIN_ID = SupportedChainId.SEPOLIA
-
-config = OrderBookAPIConfigFactory.get_config("prod", CHAIN_ID)
-ORDER_BOOK_API = OrderBookApi(config)
 
 load_dotenv()
 
@@ -48,77 +29,13 @@ if not PRIVATE_KEY:
 ACCOUNT = Account.from_key(PRIVATE_KEY)
 
 
-async def get_order_quote(
-    order_quote_request: OrderQuoteRequest, order_side: OrderQuoteSide1
-) -> OrderQuoteResponse:
-    return await ORDER_BOOK_API.post_quote(order_quote_request, order_side)
-
-
-def sign_order(order: Order) -> EcdsaSignature:
-    order_domain = domain(
-        chain=CHAIN, verifying_contract=CowContractAddress.SETTLEMENT_CONTRACT.value
-    )
-
-    return _sign_order(order_domain, order, ACCOUNT, SigningScheme.EIP712)
-
-
-async def post_order(order: Order, signature: EcdsaSignature) -> UID:
-    order_creation = OrderCreation(
-        **{
-            "from": ACCOUNT.address,
-            "sellToken": order.sellToken,
-            "buyToken": order.buyToken,
-            "sellAmount": order.sellAmount,
-            "feeAmount": order.feeAmount,
-            "buyAmount": order.buyAmount,
-            "validTo": order.validTo,
-            "kind": order.kind,
-            "partiallyFillable": order.partiallyFillable,
-            "appData": order.appData,
-            "signature": signature.data,
-            "signingScheme": "eip712",
-            "receiver": order.receiver,
-        },
-    )
-    return await ORDER_BOOK_API.post_order(order_creation)
-
-
-async def main():
-    order_quote_request = OrderQuoteRequest(
-        **{
-            "sellToken": SELL_TOKEN,
-            "buyToken": BUY_TOKEN,
-            "from": ACCOUNT.address,
-        }
-    )
-    order_side = OrderQuoteSide1(
-        kind=OrderQuoteSideKindSell.sell,
-        sellAmountBeforeFee=TokenAmount(SELL_AMOUNT_BEFORE_FEE),
-    )
-
-    order_quote = await get_order_quote(order_quote_request, order_side)
-
-    order_quote_dict = json.loads(order_quote.quote.model_dump_json(by_alias=True))
-    order = Order(
-        **{
-            "sellToken": SELL_TOKEN,
-            "buyToken": BUY_TOKEN,
-            "receiver": ACCOUNT.address,
-            "validTo": order_quote_dict["validTo"],
-            "appData": "0x0000000000000000000000000000000000000000000000000000000000000000",
-            "sellAmount": SELL_AMOUNT_BEFORE_FEE,  # Since it is a sell order, the sellAmountBeforeFee is the same as the sellAmount
-            "buyAmount": order_quote_dict["buyAmount"],
-            "feeAmount": "0",  # CoW Swap does not charge fees
-            "kind": ORDER_KIND,
-            "sellTokenBalance": "erc20",
-            "buyTokenBalance": "erc20",
-        }
-    )
-
-    signature = sign_order(order)
-    order_uid = await post_order(order, signature)
-    print(f"order posted on link: {ORDER_BOOK_API.get_order_link(order_uid)}")
-
-
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(
+        swap_tokens(
+            amount=SELL_AMOUNT_BEFORE_FEE,
+            account=ACCOUNT,
+            chain=CHAIN,
+            sell_token=SELL_TOKEN,
+            buy_token=BUY_TOKEN,
+        )
+    )
