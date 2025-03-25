ea664ad4c38012220347496fdfd77d8c956f527e
Peter Jung <peter@jung.ninja>
2025-03-25 08:27:30 +0100

    Safe support in swap_tokens

--- cowdao_cowpy/cow/swap.py
+++ cowdao_cowpy/cow/swap.py
@@ -3,7 +3,13 @@ from cowdao_cowpy.common.config import SupportedChainId
 from cowdao_cowpy.common.constants import CowContractAddress
 from cowdao_cowpy.contracts.domain import domain
 from cowdao_cowpy.contracts.order import Order
-from cowdao_cowpy.contracts.sign import EcdsaSignature, SigningScheme
+from cowdao_cowpy.contracts.sign import (
+    EcdsaSignature,
+    SigningScheme,
+    PreSignSignature,
+    Signature,
+)
+from cowdao_cowpy.common.constants import ZERO_APP_DATA
 from cowdao_cowpy.contracts.sign import sign_order as _sign_order
 from cowdao_cowpy.order_book.api import OrderBookApi
 from cowdao_cowpy.order_book.config import Envs, OrderBookAPIConfigFactory
@@ -34,6 +40,7 @@ async def swap_tokens(
     chain: Chain,
     sell_token: ChecksumAddress,
     buy_token: ChecksumAddress,
+    safe_address: ChecksumAddress | None = None,
     app_data: str = ZERO_APP_DATA,
     env: Envs = "prod",
     slippage_tolerance: float = 0.005,
@@ -47,7 +54,7 @@ async def swap_tokens(
     order_quote_request = OrderQuoteRequest(
         sellToken=sell_token,
         buyToken=buy_token,
-        from_=account._address,  # type: ignore # pyright doesn't recognize `populate_by_name=True`.
+        from_=safe_address if safe_address is not None else account._address,  # type: ignore # pyright doesn't recognize `populate_by_name=True`.
     )
     order_side = OrderQuoteSide1(
         kind=OrderQuoteSideKindSell.sell,
@@ -58,7 +65,7 @@ async def swap_tokens(
     order = Order(
         sell_token=sell_token,
         buy_token=buy_token,
-        receiver=account.address,
+        receiver=safe_address if safe_address is not None else account.address,
         valid_to=order_quote.quote.validTo,
         app_data=app_data,
         sell_amount=str(
@@ -73,8 +80,17 @@ async def swap_tokens(
         buy_token_balance="erc20",
     )
 
-    signature = sign_order(chain, account, order)
-    order_uid = await post_order(account, order, signature, order_book_api)
+    signature = (
+        PreSignSignature(
+            scheme=SigningScheme.PRESIGN,
+            data=safe_address,
+        )
+        if safe_address is not None
+        else sign_order(chain, account, order)
+    )
+    order_uid = await post_order(
+        account, safe_address, order, signature, order_book_api
+    )
     order_link = order_book_api.get_order_link(order_uid)
 
     return CompletedOrder(uid=order_uid, url=order_link)
@@ -98,12 +114,13 @@ def sign_order(chain: Chain, account: LocalAccount, order: Order) -> EcdsaSignat
 
 async def post_order(
     account: LocalAccount,
+    safe_address: ChecksumAddress | None,
     order: Order,
-    signature: EcdsaSignature,
+    signature: Signature,
     order_book_api: OrderBookApi,
 ) -> UID:
     order_creation = OrderCreation(
-        from_=account.address,  # type: ignore # pyright doesn't recognize `populate_by_name=True`.
+        from_=safe_address if safe_address is not None else account.address,  # type: ignore # pyright doesn't recognize `populate_by_name=True`.
         sellToken=order.sellToken,
         buyToken=order.buyToken,
         sellAmount=str(order.sellAmount),
@@ -114,7 +131,7 @@ async def post_order(
         partiallyFillable=order.partiallyFillable,
         appData=order.appData,
         signature=signature.data,
-        signingScheme="eip712",
+        signingScheme=signature.scheme.name.lower(),
         receiver=order.receiver,
     )
     return await order_book_api.post_order(order_creation)
