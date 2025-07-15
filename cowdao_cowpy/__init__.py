1f6f91b83c9dc321cd31cc616f65210590ef8ee6
mfw78 <53399572+mfw78@users.noreply.github.com>
2025-07-15 20:34:08 +0000

    chore: automatically append version to utm (#64)

--- cowdao_cowpy/__init__.py
+++ cowdao_cowpy/__init__.py
@@ -1,3 +1,10 @@
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version("cowdao-cowpy")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "development"
+
 from cowdao_cowpy.cow.swap import swap_tokens
 
-__all__ = ["swap_tokens"]
+__all__ = ["swap_tokens", "__version__"]
