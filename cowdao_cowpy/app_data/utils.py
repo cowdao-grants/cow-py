cbe683578a1d9f6eebf1a2df2c7f500409d49b44
8ball030 <8baller@station.codes>
2025-07-25 01:45:40 +0100

    feat:refactor-for-default-app-code

--- cowdao_cowpy/app_data/utils.py
+++ cowdao_cowpy/app_data/utils.py
@@ -1,8 +1,13 @@
+import asyncio
 from typing import Any, Dict
+from cowdao_cowpy.order_book.api import OrderBookApi
+from cowdao_cowpy.order_book.config import OrderBookAPIConfigFactory
 from cowdao_cowpy.order_book.generated.model import AppData, AppDataHash, AppDataObject
 import httpx
 from multiformats import CID
 from collections.abc import Mapping
+from cowdao_cowpy.common.api.api_base import Envs
+from cowdao_cowpy.common.config import SupportedChainId
 
 from web3 import Web3
 
@@ -66,14 +71,14 @@ class QuoteFee:
 
 
 def generate_app_data(
-    app_code: str,
+    app_code: str = None,
     partner_fee: PartnerFee = None,
     referrer_address: str = None,
     version: str = "1.3.0",
     hooks_version: str = "0.1.0",
+    graffiti: str | None = None,
 ) -> CreateAppData:
     app_data_doc = {
-        "appCode": app_code,
         "metadata": {
             "hooks": {
                 "version": hooks_version,
@@ -82,9 +87,9 @@ def generate_app_data(
                 "utmSource": "cowmunity",
                 "utmMedium": f"cow-py@{__version__}",
                 "utmCampaign": "developer-cohort",
-                "utmContent": "🥩📢🌀🐮 'MÖØ'",
-                "utmTerm": "python"
-        }
+                "utmContent": "🥩📢🌀🐮 'MÖØ'" if not graffiti else graffiti,
+                "utmTerm": "python",
+            },
         },
         "version": version,
     }  # compact encoding to match JS behavior
@@ -96,9 +101,67 @@ def generate_app_data(
     if partner_fee:
         app_data_doc["metadata"]["partnerFee"] = (asdict(partner_fee),)
 
+    if app_code:
+        app_data_doc["appCode"] = app_code
     stringified_data = stringify_deterministic(app_data_doc)
     app_data_hash = keccak256(stringified_data.encode("utf-8"))
     return CreateAppData(
         AppDataObject(fullAppData=AppData(stringified_data)),
         AppDataHash(root=app_data_hash),
     )
+
+
+def check_app_data_exists(orderbook: OrderBookApi, app_data_hash: AppDataHash) -> bool:
+    try:
+        # Attempt to fetch the app data from the orderbook
+        asyncio.run(orderbook.get_app_data(app_data_hash))
+    except Exception as e:  # noqa
+        print(f"App data not found: {e}")
+        return False
+    return True
+
+
+def build_and_post_app_data(
+    orderbook: OrderBookApi,
+    app_code: str | None = None,
+    referrer_address: str | None = None,
+    partner_fee: PartnerFee | None = None,
+    graffiti: str | None = None,
+) -> str:
+    create_app_data = generate_app_data(
+        app_code=app_code,
+        referrer_address=referrer_address,
+        partner_fee=partner_fee,
+        graffiti=graffiti,
+    )
+    if not check_app_data_exists(orderbook, create_app_data.app_data_hash):
+        print("App data does not exist, uploading... to ", orderbook.config.chain_id)
+        asyncio.run(
+            orderbook.put_app_data(
+                create_app_data.app_data_object, create_app_data.app_data_hash
+            )
+        )
+    return create_app_data.app_data_hash.root
+
+
+def build_all_app_codes(
+    env: Envs = "prod",
+    app_code: str = "cowdao_cowpy",
+    graffiti: str | None = None,
+    referrer_address: str | None = None,
+    partner_fee: PartnerFee | None = None,
+) -> str:
+    """
+    Builds and posts app data for all supported chain IDs.
+    """
+    for chain_id in SupportedChainId:
+        order_book_api = OrderBookApi(
+            OrderBookAPIConfigFactory.get_config(env, chain_id)
+        )
+        app_data_hash = build_and_post_app_data(
+            order_book_api, app_code, referrer_address, partner_fee, graffiti
+        )
+    return app_data_hash  # type: ignore
+
+
+DEFAULT_APP_DATA_HASH = build_all_app_codes()
