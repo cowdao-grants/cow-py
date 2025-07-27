7f8b7caadc6662853940baa98e904a61f69764b4
8ball030 <35799987+8ball030@users.noreply.github.com>
2025-07-27 11:36:28 +0100

    feat:refactor-for-default-app-code (#66)
    * feat:refactor-for-default-app-code
    
    * refactor:small-fixes-and-cleanup
    
    ---------
    
    Co-authored-by: 8ball030 <8baller@station.codes>

--- tests/e2e/test_upload_app_data.py
+++ tests/e2e/test_upload_app_data.py
@@ -2,12 +2,15 @@
 Tests for uploading application data to the COW server.
 """
 
-from examples.appdata import main
+from cowdao_cowpy.app_data.utils import DEFAULT_APP_DATA_HASH, build_all_app_codes
 
 
 def test_upload_app_data():
     """
     Test the upload of application data to the COW server.
     """
-    app_data_hash = main()
+    app_data_hash = build_all_app_codes()
     assert isinstance(app_data_hash, str)
+    assert (
+        app_data_hash == DEFAULT_APP_DATA_HASH
+    ), "App data hash does not match the default value."
